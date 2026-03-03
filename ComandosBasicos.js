895ce8d6893e5c18fab1336ecc05f56f0f14efcb
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2026-03-03 11:18:20 -0300

    Add basic commands for user input and display

---
+++ ComandosBasicos.js
@@ -0,0 +1,15 @@
+
+console.log("Bem-vindo ao sistema!");
+
+let nome = prompt("Digite seu nome:");
+
+let idade = parseInt(prompt("Digite sua idade:"));
+
+let altura = parseFloat(prompt("Digite sua altura (ex: 1.75):"));
+
+console.log(
+    "Nome: " + nome +
+    "\nIdade: " + idade +
+    "\nAltura: " + altura
+);
+
