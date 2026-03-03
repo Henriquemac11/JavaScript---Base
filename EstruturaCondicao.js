70dab0734ec400f5e0d53fe43e55de9afede71af
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2026-03-03 11:20:31 -0300

    Add conditional structure examples in EstruturaCondicao.js

---
+++ EstruturaCondicao.js
@@ -0,0 +1,26 @@
+
+//IF, ELSE e ELSE IF
+let nota = 7;
+
+if (nota >= 7) {
+    console.log("Aprovado");
+} else if (nota >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
+
+//SWITCH
+
+let dia = 2;
+
+switch (dia) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda");
+        break;
+    default:
+        console.log("Outro dia");
+}
