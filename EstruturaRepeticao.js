e34de275e271726c49224ed50d26800acdc9a49a
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2026-03-03 11:22:17 -0300

    Add basic loop structures in EstruturaRepeticao.js

---
+++ EstruturaRepeticao.js
@@ -0,0 +1,24 @@
+//FOR
+
+for (let i = 1; i <= 5; i++) {
+    console.log("Número:", i);
+}
+
+//WHILE 
+
+let numero = 1;
+
+while (numero <= 5) {      //Repete enquanto a condição for verdadeira.
+    console.log(numero);
+    numero++;
+}
+
+//DO WHILE
+let x = 1;
+
+do {
+    console.log(x);
+    x++;
+} while (x <= 5);
+
+
