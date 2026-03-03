1c0332fca2d715c754f47a6c1a2987d5c9774a19
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2026-03-03 11:15:28 -0300

    Create Variaveis.js

---
+++ Variaveis.js
@@ -0,0 +1,13 @@
+// Variaveis sao espacos na memoria usados para guardar informacoes.
+
+let idade = 18;
+var cidade = "Cidade";
+const nome = "Henrique";
+
+// Tipos de Dados
+let nome2 = "Allan";   
+let idade2 = 18;          
+let aprovado = true;     
+let lista = [1, 2, 3];   
+let pessoa = { nome: "Allan", idade: 18 }; 
+
